b6d8f98aa319489382e6e765c5c5aa94707f1a82
schimmelc <schimmelc4422@my.uwstout.edu>
2015-04-18 17:03:46 -0500

    updates for level ui

---
+++ ProjectNoFrictionCapers/Assets/Scripts/GameAndUIController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class GameAndUIController : MonoBehaviour
+{
+    public GameObject levelComplete;
+
+    void Awake()
+    {
+        //if (Advertisement.isSupported)
+        //{
+        //    Debug.Log("platform supported for ads");
+        //    Advertisement.allowPrecache = true;
+        //    Advertisement.Initialize("32757", false);
+        //}
+        //else
+        //{
+        //    Debug.Log("Platform not supported");
+        //}
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            levelComplete.SetActive(true);
+            if (Advertisement.isReady())
+            {
+                Advertisement.Show();
+            }
+        }
+    }
+
+    public void UnPause()
+    {
+
+    }
+
+    public void Pause()
+    {
+
+    }
+}
