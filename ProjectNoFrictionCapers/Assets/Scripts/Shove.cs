37598121a0bb505aeda7c160e6ac11b9b5a32624
polseane9067 <polseane9067@my.uwstout.edu>
2015-04-18 00:51:37 -0500

    Tilt stuff

---
+++ ProjectNoFrictionCapers/Assets/Scripts/Shove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shove : MonoBehaviour {
+
+    public bool push;
+    public Rigidbody rb;
+    public float startTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(push == true && (Time.time - startTime) >= 1 && startTime != 0)
+        {
+            startTime = 0;
+            push = false;
+        }
+        if (push == true && startTime == 0)
+        {
+            rb.AddForce(Vector3.forward * 100);
+            startTime = Time.time;
+        }
+	}
+}
---
+++ ProjectNoFrictionCapers/Assets/Scripts/Tilt.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(0, 0, -0.2f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-0.2f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(0, 0, 0.2f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0.2f, 0, 0);
+        }
+        if (!Input.anyKey)
+        {
+            float rate = (transform.rotation.x + transform.rotation.y + transform.rotation.z)/3;
+            print(rate);
+            transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(0, 0, 0, 1), Time.time * rate);
+        }
+	}
+}
