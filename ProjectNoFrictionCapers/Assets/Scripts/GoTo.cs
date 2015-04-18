42fcf66cd024f2adfba8c5b21eeaa9f18b21a379
schimmelc <schimmelc4422@my.uwstout.edu>
2015-04-17 21:57:31 -0500

    Start Screen start

---
+++ ProjectNoFrictionCapers/Assets/Scripts/GoTo.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoTo : MonoBehaviour {
+
+    public GameObject MainMenuPanel;
+    public GameObject HowToPlayPanel;
+    public GameObject CreditsPanel;
+
+    void Start()
+    {
+        if(MainMenuPanel != null)
+        {
+            
+        }
+    }
+
+    public void HowToPlay()
+    {
+        
+    }
+
+
+    public void StartScreen()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel("StartScreen");
+    }
+
+    public void Credits()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
