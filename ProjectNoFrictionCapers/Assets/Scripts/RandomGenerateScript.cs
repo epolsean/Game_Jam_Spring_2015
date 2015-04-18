79b64beb222348527d8d67768e412540106b68ab
polseane9067 <polseane9067@my.uwstout.edu>
2015-04-18 18:36:15 -0500

    Fun Collision Stuff

--- ProjectNoFrictionCapers/Assets/Scripts/RandomGenerateScript.cs
+++ ProjectNoFrictionCapers/Assets/Scripts/RandomGenerateScript.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class RandomGenerateScript : MonoBehaviour {
 
-    public GameObject spawn;
+    public GameObject[] spawn;
+    public GameObject parent;
+    public int Spawns;
 
     GameObject[][] slots = new GameObject[10][];
+    int[][] slotsTaken = new int[10][];
     public GameObject[] slot0;
     public GameObject[] slot1;
     public GameObject[] slot2;
@@ -29,9 +32,19 @@ public class RandomGenerateScript : MonoBehaviour {
         slots[7] = slot7;
         slots[8] = slot8;
         slots[9] = slot9;
+        slotsTaken[0] = new int[10];
+        slotsTaken[1] = new int[10];
+        slotsTaken[2] = new int[10];
+        slotsTaken[3] = new int[10];
+        slotsTaken[4] = new int[10];
+        slotsTaken[5] = new int[10];
+        slotsTaken[6] = new int[10];
+        slotsTaken[7] = new int[10];
+        slotsTaken[8] = new int[10];
+        slotsTaken[9] = new int[10];
 
-        int rand1 = Random.Range(0,20);
-
+        int rand1 = Random.Range(5,40);
+        Spawns = rand1;
         for (int i = 0; i < rand1; i++)
         {
             int rand2 = Random.Range(0, 9);
@@ -42,8 +55,19 @@ public class RandomGenerateScript : MonoBehaviour {
                 {
                     if (j == rand2 && k == rand3)
                     {
-                        Quaternion randRot = new Quaternion(Random.Range(0f,1f),0,Random.Range(0f,1f),1);
-                        Instantiate(spawn, slots[j][k].transform.position, randRot);
+                        if (slotsTaken[j][k] != 1)
+                        {
+                            int rand4 = Random.Range(0, spawn.Length);
+                            Quaternion randRot = new Quaternion(0, Random.Range(0f, 1f), 0, 1);
+                            GameObject newObj = (GameObject)Instantiate(spawn[rand4], slots[j][k].transform.position, randRot);
+                            newObj.transform.parent = parent.transform;
+                            newObj.transform.position = new Vector3(newObj.transform.position.x, parent.transform.position.y, newObj.transform.position.z);
+                            slotsTaken[j][k] = 1;
+                        }
+                        else
+                        {
+                            rand3 = Random.Range(k, 9);
+                        }
                     }
                 }
             }
