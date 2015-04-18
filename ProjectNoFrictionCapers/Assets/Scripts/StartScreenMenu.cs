57d7a14b0baa1c091fc44062027bf7c6192fcf66
schimmelc <schimmelc4422@my.uwstout.edu>
2015-04-18 00:56:54 -0500

    start screen changes

---
+++ ProjectNoFrictionCapers/Assets/Scripts/StartScreenMenu.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartScreenMenu : MonoBehaviour 
+{
+
+    public GameObject MainMenuPanel;
+    public GameObject HowToPlayPanel;
+    public GameObject CreditsPanel;
+
+    void Awake() 
+    {
+        if (Advertisement.isSupported) 
+        {
+            Debug.Log("platform supported for ads");
+            Advertisement.allowPrecache = true;
+            Advertisement.Initialize ("32757",false);
+        } 
+        else 
+        {
+            Debug.Log("Platform not supported");
+        }
+    }
+
+    void Start()
+    {
+        GoToMainPanel();
+    }
+
+    public void GoToHowToPlay()
+    {
+        if (MainMenuPanel != null)
+        {
+            MainMenuPanel.SetActive(false);
+        }
+        if (HowToPlayPanel != null)
+        {
+            HowToPlayPanel.SetActive(true);
+        }
+        if (CreditsPanel != null)
+        {
+            CreditsPanel.SetActive(false);
+        }
+    }
+
+
+    public void GoToMainPanel()
+    {
+        if (MainMenuPanel != null)
+        {
+            MainMenuPanel.SetActive(true);
+        }
+        if (HowToPlayPanel != null)
+        {
+            HowToPlayPanel.SetActive(false);
+        }
+        if (CreditsPanel != null)
+        {
+            CreditsPanel.SetActive(false);
+        }
+    }
+
+    public void GoToLevelSelect()
+    {
+        if (Advertisement.isReady()) 
+        {
+            Advertisement.Show(); 
+        } 
+        //Application.LoadLevel("StartScreen");
+    }
+
+    public void GoToCredits()
+    {
+        if (MainMenuPanel != null)
+        {
+            MainMenuPanel.SetActive(false);
+        }
+        if (HowToPlayPanel != null)
+        {
+            HowToPlayPanel.SetActive(false);
+        }
+        if (CreditsPanel != null)
+        {
+            CreditsPanel.SetActive(true);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
