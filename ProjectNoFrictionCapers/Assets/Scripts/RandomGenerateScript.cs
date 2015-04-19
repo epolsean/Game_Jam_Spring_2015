bfa3defbefebc2b17fed9b98714a28e2e0bce841
polseane9067 <polseane9067@my.uwstout.edu>
2015-04-19 15:08:51 -0500

    Just a few things

--- ProjectNoFrictionCapers/Assets/Scripts/RandomGenerateScript.cs
+++ ProjectNoFrictionCapers/Assets/Scripts/RandomGenerateScript.cs
@@ -35,29 +35,28 @@ public class RandomGenerateScript : MonoBehaviour {
 	void Start () {
         slots = new GameObject[columns][];
         slotsTaken = new int[columns][];
-        slots[0] = slot0;
-        slots[1] = slot1;
-        slots[2] = slot2;
-        slots[3] = slot3;
-        slots[4] = slot4;
-        slots[5] = slot5;
-        slots[6] = slot6;
-        slots[7] = slot7;
-        slots[8] = slot8;
-        slots[9] = slot9;
-        slotsTaken[0] = new int[slot0.Length];
-        slotsTaken[1] = new int[slot0.Length];
-        slotsTaken[2] = new int[slot0.Length];
-        slotsTaken[3] = new int[slot0.Length];
-        slotsTaken[4] = new int[slot0.Length];
-        slotsTaken[5] = new int[slot0.Length];
-        slotsTaken[6] = new int[slot0.Length];
-        slotsTaken[7] = new int[slot0.Length];
-        slotsTaken[8] = new int[slot0.Length];
-        slotsTaken[9] = new int[slot0.Length];
+        if(columns >= 1 && slot0 != null)    {  slots[0] = slot0;     slotsTaken[0] = new int[slot0.Length];    }
+        if(columns >= 2 && slot1 != null)    {  slots[1] = slot1;     slotsTaken[1] = new int[slot1.Length];    }
+        if(columns >= 3 && slot2 != null)    {  slots[2] = slot2;     slotsTaken[2] = new int[slot2.Length];    }
+        if(columns >= 4 && slot3 != null)    {  slots[3] = slot3;     slotsTaken[3] = new int[slot3.Length];    }
+        if(columns >= 5 && slot4 != null)    {  slots[4] = slot4;     slotsTaken[4] = new int[slot4.Length];    }
+        if(columns >= 6 && slot5 != null)    {  slots[5] = slot5;     slotsTaken[5] = new int[slot5.Length];    }
+        if(columns >= 7 && slot6 != null)    {  slots[6] = slot6;     slotsTaken[6] = new int[slot6.Length];    }
+        if(columns >= 8 && slot7 != null)    {  slots[7] = slot7;     slotsTaken[7] = new int[slot7.Length];    }
+        if(columns >= 9 && slot8 != null)    {  slots[8] = slot8;     slotsTaken[8] = new int[slot8.Length];    }
+        if(columns >= 10 && slot9 != null)   {  slots[9] = slot9;     slotsTaken[9] = new int[slot9.Length];    }
+        if(columns >= 11 && slot10 != null)  {  slots[10] = slot10;   slotsTaken[10] = new int[slot10.Length];  }
+        if(columns >= 12 && slot11 != null)  {  slots[11] = slot11;   slotsTaken[11] = new int[slot11.Length];  }
+        if(columns >= 13 && slot12 != null)  {  slots[12] = slot12;   slotsTaken[12] = new int[slot12.Length];  }
+        if(columns >= 14 && slot13 != null)  {  slots[13] = slot13;   slotsTaken[13] = new int[slot13.Length];  }
+        if(columns >= 15 && slot14 != null)  {  slots[14] = slot14;   slotsTaken[14] = new int[slot14.Length];  }
+        if(columns >= 16 && slot15 != null)  {  slots[15] = slot15;   slotsTaken[15] = new int[slot15.Length];  }
+        if(columns >= 17 && slot16 != null)  {  slots[16] = slot16;   slotsTaken[16] = new int[slot16.Length];  }
+        if(columns >= 18 && slot17 != null)  {  slots[17] = slot17;   slotsTaken[17] = new int[slot17.Length];  }
+        if(columns >= 19 && slot18 != null)  {  slots[18] = slot18;   slotsTaken[18] = new int[slot18.Length];  }
+        if(columns >= 20 && slot19 != null)  {  slots[19] = slot19;   slotsTaken[19] = new int[slot19.Length];  }
 
-        int rand1 = Random.Range(5,40);
-        Spawns = rand1;
+        int rand1 = Random.Range(5,Spawns);
         for (int i = 0; i < rand1; i++)
         {
             int rand2 = Random.Range(0, 9);
@@ -68,6 +67,10 @@ public class RandomGenerateScript : MonoBehaviour {
                 {
                     if (j == rand2 && k == rand3)
                     {
+                        /*if (Physics.OverlapSphere(slots[j][k].transform.position, 1.8f).Length > 2)
+                        {
+                            slotsTaken[j][k] = 1;
+                        }*/
                         if (slotsTaken[j][k] != 1)
                         {
                             int rand4 = Random.Range(0, spawn.Length);
