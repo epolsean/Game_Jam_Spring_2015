6aebfc4f43a331196178c8ba110c1450d1b7a06b
blochowcizr <blochowiczr@yahoo.com>
2015-04-19 08:46:47 -0500

    wall pressure plate!!!!
    we now not only have a floor pressure plate but a wall pressure plate as
    well!!!!!!! the doors will close again if the weight is taken off.

--- ProjectNoFrictionCapers/Assets/Scripts/MobileTiltScript.cs
+++ ProjectNoFrictionCapers/Assets/Scripts/MobileTiltScript.cs
@@ -6,32 +6,49 @@ public class MobileTiltScript : MonoBehaviour
 
     public int turnSpeed;
     public int turnBuffer; 
+	public GameObject ChildGroup; 
+
+	public float Q_y; 
     
     // Use this for initialization
 	void Start () {
-	
+		ChildGroup.transform.Rotate(new Vector3(0,1,0), 90); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(0, 0, -0.2f);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(-0.2f, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(0, 0, 0.2f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0.2f, 0, 0);
-        }
+		if (Input.GetKey(KeyCode.W))
+		{
+			if (transform.rotation.z > -0.083333333333333f && transform.rotation.x > -0.083333333333333f && transform.rotation.x < 0.083333333333333f)
+			{
+				transform.Rotate(0, 0, -0.2f);
+			}
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			if (transform.rotation.x > -0.083333333333333f && transform.rotation.z > -0.083333333333333f && transform.rotation.z < 0.083333333333333f)
+			{
+				transform.Rotate(-0.2f, 0, 0);
+			}
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			if (transform.rotation.z < 0.083333333333333f && transform.rotation.x > -0.083333333333333f && transform.rotation.x < 0.083333333333333f)
+			{
+				transform.Rotate(0, 0, 0.2f);
+			}
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			if (transform.rotation.x < 0.083333333333333f && transform.rotation.z > -0.083333333333333f && transform.rotation.z < 0.083333333333333f)
+			{
+				transform.Rotate(0.2f, 0, 0);
+			}
+		}
+		//transform.rotation = new Quaternion(transform.rotation.x, .7071067985f, transform.rotation.z, transform.rotation.w);
+        
 
-        transform.rotation = new Quaternion(Input.acceleration.x, 0, Input.acceleration.y, turnBuffer);
+		transform.rotation = new Quaternion(Input.acceleration.y, Q_y, -Input.acceleration.x, turnBuffer);
         print(Input.acceleration); 
 
         /*if(Input.acceleration.y  > 0)
---
+++ ProjectNoFrictionCapers/Assets/Scripts/WallPressurePlateScript.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallPressurePlateScript : MonoBehaviour {
+
+	public GameObject plateObj;
+	
+	public GameObject Door01; 
+	public GameObject D01_LerpPoint; 
+	public GameObject D01_startPoint; 
+	public GameObject Door02; 
+	public GameObject D02_LerpPoint;
+	public GameObject D02_startPoint; 
+
+	public bool DoorsOpen = false; 
+	
+	public float smooth = 3.0F;
+	public bool isLerpin = false;
+	public float LerpTimer = 0.0f;
+	public float LerpsUpTime = 1.5f; 
+	public float speed = 3.0F;
+	private float startTime;
+	private float journeyLength;
+	
+	// Use this for initialization
+	void Start () {
+		//D01_startPoint = Door01.transform; 
+		//D02_startPoint = Door02.transform; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLerpin)
+		{
+			if(DoorsOpen == false)
+			{
+				OpenDoors();
+			}
+			else{
+				CloseDoors(); 
+			}
+			LerpTimer += Time.deltaTime; 
+			if(LerpTimer >= LerpsUpTime)
+			{
+				endLerp();
+			}
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "plateTrigger")
+		{
+			other.transform.position = new Vector3(this.transform.position.x, other.transform.position.y, this.transform.position.z);
+			other.transform.rotation = this.transform.rotation; 
+			//other.GetComponent<Rigidbody>().isKinematic = true;
+			plateObj.transform.position -= new Vector3(0, 0, -0.1f);  
+			isLerpin = true; 
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "plateTrigger") {
+			print ("Close The Doors!"); 
+			other.GetComponent<Rigidbody>().isKinematic = false; 
+			plateObj.transform.position += new Vector3(0,0,-0.1f);
+			isLerpin = true;
+
+		}
+	}
+	
+	public void endLerp()
+	{
+		isLerpin = false; 
+		if (DoorsOpen) {
+			DoorsOpen = false; 
+		} else {
+			DoorsOpen = true; 
+		}
+		LerpTimer = 0; 
+	}
+	
+	public void OpenDoors()
+	{
+		OpenDoor (Door01, D01_LerpPoint); 
+		OpenDoor (Door02, D02_LerpPoint); 
+	}
+
+	public void CloseDoors()
+	{
+		CloseDoor (Door01, D01_startPoint); 
+		CloseDoor (Door02, D02_startPoint); 
+	}
+	
+	public void OpenDoor(GameObject Door, GameObject NewTarget)
+	{
+		Door.transform.position = Vector3.Lerp(Door.transform.position, NewTarget.transform.position, smooth*Time.deltaTime);
+	}
+
+	public void CloseDoor(GameObject Door, GameObject NewTarget)
+	{
+		print ("closing"); 
+		Door.transform.position = Vector3.Lerp(Door.transform.position, NewTarget.transform.position, smooth*Time.deltaTime);
+	}
+
+
+}
