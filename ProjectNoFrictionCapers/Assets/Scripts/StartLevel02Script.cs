0a345e94bb561ba49da19d9504655f6fe31f2d6c
blochowcizr <blochowiczr@yahoo.com>
2015-04-19 16:42:34 -0500

    added cop attack lol

---
+++ ProjectNoFrictionCapers/Assets/Scripts/StartLevel02Script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLevel02Script : MonoBehaviour {
+
+	public GameObject[] 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
